0f0a726ae1c93111c4a62cc4f7101e505803e092
jcagz96 <jcagz@iscte-iul.pt>
2020-03-26 20:48:29 +0000

    created mobile app

--- mobile/App.js
+++ mobile/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import 'intl';
+import 'intl/locale-data/jsonp/pt-PT';
 
+import React from 'react';
 import Routes from './src/routes';
 
 export default function App() {
